038d8a49e96271f91ee3597a920c8bb40c13fc40
Nhat Nguyen <nhat.nguyen@elastic.co>
2026-01-22 13:53:22 -0800

    large hash

--- x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/aggregation/blockhash/LongIntBlockHash.java
+++ x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/aggregation/blockhash/LongIntBlockHash.java
@@ -41,7 +41,7 @@ public final class LongIntBlockHash extends BlockHash {
         this.intChannel = intChannel;
         this.reverseOutput = reverseOutput;
         this.emitBatchSize = emitBatchSize;
-        this.directHash = new LongLongHash(PageCacheRecycler.LONG_PAGE_SIZE, blockFactory.bigArrays());
+        this.directHash = new LongLongHash(PageCacheRecycler.PAGE_SIZE_IN_BYTES, blockFactory.bigArrays());
     }
 
     @Override
