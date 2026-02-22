ce9335f1b845ac1875452cc84349119b46e4bd40
Nhat Nguyen <nhat.nguyen@elastic.co>
2026-02-22 00:38:49 -0800

    No waiting

--- x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/lucene/query/DocPartitioningQueryCache.java
+++ x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/lucene/query/DocPartitioningQueryCache.java
@@ -28,7 +28,6 @@ import java.util.concurrent.atomic.AtomicInteger;
  * This is best-effort as other threads might also fall back to an uncached scorer.
  */
 final class DocPartitioningQueryCache implements QueryCache {
-    private final Map<Object, SubscribableListener<Void>> cachingListeners = ConcurrentCollections.newConcurrentMap();
     private final QueryCache actual;
 
     DocPartitioningQueryCache(QueryCache actual) {
@@ -48,49 +47,22 @@ final class DocPartitioningQueryCache implements QueryCache {
      * or {@code null} if no caching is in progress for this leaf.
      */
     SubscribableListener<Void> blockedOnCaching(LeafReaderContext leaf) {
-        SubscribableListener<Void> listener = cachingListeners.get(leaf.id());
-        if (listener == null || listener.isDone()) {
-            return null;
-        }
-        return listener;
+        return null;
     }
 
-    private static SubscribableListener<Void> combine(SubscribableListener<Void> first, SubscribableListener<Void> second) {
-        SubscribableListener<Void> combined = new SubscribableListener<>();
-        AtomicInteger counter = new AtomicInteger(2);
-        ActionListener<Void> onComplete = ActionListener.running(() -> {
-            if (counter.decrementAndGet() == 0) {
-                combined.onResponse(null);
-            }
-        });
-        first.addListener(onComplete);
-        second.addListener(onComplete);
-        return combined;
-    }
 
-    class WrappedWeight extends IndicesQueryCache.OptionalCachingWeight {
+    static class WrappedWeight extends IndicesQueryCache.OptionalCachingWeight {
         private final Set<Object> cached = ConcurrentCollections.newConcurrentSet();
 
         WrappedWeight(Weight weight) {
             super(weight);
         }
 
-        private void maybeRemoveCachingListener(LeafReaderContext leaf) {
-            cachingListeners.compute(leaf.id(), (k, curr) -> curr == null || curr.isDone() ? null : curr);
-        }
-
         @Override
         public Releasable startCaching(LeafReaderContext leaf) {
-            final SubscribableListener<Void> listener = new SubscribableListener<>();
-            cachingListeners.compute(leaf.id(), (k, curr) -> curr == null || curr.isDone() ? listener : combine(curr, listener));
             if (cached.add(leaf.id())) {
-                return () -> {
-                    listener.onResponse(null);
-                    maybeRemoveCachingListener(leaf);
-                };
+                return () -> {};
             }
-            listener.onResponse(null);
-            maybeRemoveCachingListener(leaf);
             return null;
         }
     }
